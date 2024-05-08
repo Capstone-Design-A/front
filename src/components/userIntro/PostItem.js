8c6105d6018cd9a1fcc83ac4ffae633b5c531451
jul-ee <160218278+jul-ee@users.noreply.github.com>
2024-05-08 16:28:25 +0900

    모달창을 통한 게시물 상세 화면 설계

--- src/components/userIntro/PostItem.js
+++ src/components/userIntro/PostItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./PostItem.module.css";
 
 const PostItem = ({ post }) => {
-  const { id, imageUrl, title, content } = post;
+  const { id, imageUrl, title, content, createdAt } = post;
 
   return (
     <div className={styles.postItem}>
@@ -10,6 +10,7 @@ const PostItem = ({ post }) => {
       <div className={styles.postContent}>
         <h2 className={styles.postTitle}>{title}</h2>
         <p className={styles.postDescription}>{content}</p>
+        <p className={styles.postDescription}>{createdAt}</p>
       </div>
     </div>
   );
--- src/components/userIntro/PostList.js
+++ src/components/userIntro/PostList.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./PostList.module.css";
 
-const PostList = ({ postList }) => {
+const PostList = ({ postList, onPostClick }) => {
   return (
     <div className={styles.postListContainer}>
       {postList.map((post) => (
-        <div key={post.id} className={styles.postItem}>
+        <div
+          key={post.id}
+          className={styles.postItem}
+          onClick={() => onPostClick(post)}
+        >
           <img src={post.imageUrl} alt="Post" className={styles.postImage} />
         </div>
       ))}
