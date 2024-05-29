1db2de8ec475c35b489c87a9fa6d7fe82eb33dca
jul-ee <160218278+jul-ee@users.noreply.github.com>
2024-05-29 17:25:32 +0900

    구독 여부 확인 API 연결

--- src/components/userIntro/UserProfile.js
+++ src/components/userIntro/UserProfile.js
@@ -1,23 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./UserProfile.module.css";
-import { subscribe, unsubscribe } from "../../api/api.js";
+import {
+  subscribe,
+  unsubscribe,
+  getSubscriptionStatus,
+} from "../../api/api.js";
 
 const UserProfile = ({ user }) => {
-  const [isSubscribed, setIsSubscribed] = useState(user.isSubscribed);
+  const [isSubscribed, setIsSubscribed] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const checkSubscriptionStatus = async () => {
+      try {
+        const result = await getSubscriptionStatus(user.memberId);
+        if (result.isSuccess) {
+          setIsSubscribed(result.result.isSubscribe);
+        } else {
+          setErrorMessage(result.message);
+        }
+      } catch (error) {
+        console.error("Error checking subscription status:", error);
+        setErrorMessage("Failed to check subscription status");
+      }
+    };
+    checkSubscriptionStatus();
+  }, [user.memberId]);
 
   const handleSubscribeClick = async () => {
     try {
       if (isSubscribed) {
-        const result = await unsubscribe(2, user.fromMemberId);
+        const result = await unsubscribe(user.memberId);
         if (result.isSuccess) {
           setIsSubscribed(false);
         } else {
           setErrorMessage(result.message);
         }
       } else {
-        const result = await subscribe(2, user.fromMemberId);
+        const result = await subscribe(user.memberId);
         if (result.isSuccess) {
           setIsSubscribed(true);
         } else {
