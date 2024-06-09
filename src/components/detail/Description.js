a8c865bf74069e119505e6bf284152ae8d6c8ca4
jul-ee <160218278+jul-ee@users.noreply.github.com>
2024-06-09 22:11:14 +0900

    상품 상세 imageUrl 수정

--- src/components/detail/Description.js
+++ src/components/detail/Description.js
@@ -2,16 +2,9 @@ import React from "react";
 import styles from "./Description.module.css";
 
 function Description({ item }) {
-  const mainImageUrl =
-    item && item.imageUrls && item.imageUrls.length > 0
-      ? item.imageUrls[0]
-      : "";
-
-  console.log("item: ", item);
-  console.log("imageUrl: ", mainImageUrl);
   return (
     <div className={styles.img}>
-      <img src={mainImageUrl} alt="imageUrl" />
+      <img src={item.itemDetailsImageUrl} alt="imageUrl" />
     </div>
   );
 }
--- src/components/detail/ProductGroupDetail.js
+++ src/components/detail/ProductGroupDetail.js
@@ -46,17 +46,17 @@ function ProductGroupDetail({ item }) {
   };
 
   const totalPrice = quantity * item.price;
+  const mainImageUrl =
+    item && item.imageUrls && item.imageUrls.length > 0
+      ? item.imageUrls[0]
+      : "";
 
   return (
     <>
       <div className={styles.layout}>
         <Container className={styles.content}>
           <div className={styles.image}>
-            <img
-              className={styles.img}
-              src={item.itemDetailsImageUrl}
-              alt={item.name}
-            />
+            <img className={styles.img} src={mainImageUrl} alt={item.name} />
             <Link to={`/intro/${item.memberId}`}>
               <button className={styles.button}>판매자 소개 바로가기</button>
             </Link>
